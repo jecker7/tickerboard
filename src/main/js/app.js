9278dae052e02bee801518ff6effb031d32625b7
john ecker <jhecker123@gmail.com>
2019-05-08 20:11:38 -0400

    adding React components, missing files

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -16,16 +16,46 @@ class App extends React.Component {
                 this.state = {stocks: []};
         }
         // invoking our API after React renders our component in the DOM
+        // here we fetch an array of employees from the Spring Data REST backend,
+        // and store the array in this components state data - state data is data the component handles itself
         componentDidMount() {
+        // our function loads data using client, a Promise compliant instance of rest.js
                 client({method: 'GET', path: 'api/stocks'}).done(response => {
                         this.setState({stocks: response.entity._embedded.stocks});
                 });
         }
 
-        // render() is the API to draw our component on the screen
+        // render() is the API to draw our component on the screen - it's called after our state is updated above
+        // stock state data is used to create a <StockList /> React component
         render() {
                 return (
                         <StockList stocks = {this.state.stocks}/>
+                )
+        }
+}
+
+class StockList extends React.Component{
+        render() {
+                // using map function to transform this.props.stocks from array of stocks to an array of <Element />
+                // React components, and creating a new React component along with two properties: key, and data,
+                // which are supplied the values from employee._links.self.href and employee
+                // the "self" link is our unique key in Spring for stocks, and can be used as a unique identifier for React child nodes
+                const stocks = this.props.stocks.map(stocks =>
+
+                        <Stock key={stock._links.self.href} stock={stock}/>
+                );
+                // we'll return an HTML table wrapped around the array of stocks
+                return (
+                            <table>
+                                    <tbody>
+                                            <tr>
+                                                   <th>Company Name</th>
+                                                   <th>Symbol</th>
+                                                   <th>Price</th>
+                                            </tr>
+                                            {stocks}
+                                    </tbody>
+                            </table>
                 )
         }
 }
