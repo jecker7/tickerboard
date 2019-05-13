49c403e0001106326703af4516dd2c1f0a2f112d
john ecker <jhecker123@gmail.com>
2019-05-13 14:16:46 -0400

    price updates working

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,6 +6,7 @@ const ReactDOM = require('react-dom');
 // client also sets default Accept request header to application/hal+json
 const client = require('./client');
 
+import axios from 'axios'
 
 // top level container for all React components
 // extending React.Component to create a React component (React Components follow uppercase naming convention)
@@ -13,23 +14,45 @@ class App extends React.Component {
 
         constructor(props) {
                 super(props);
-                this.state = {stocks: []};
+                this.state = {stocks: [], oldStocks: [], stocksDictionary : {}, oldStocksDictionary: {}};
         }
+
+        fetchData = () => {
+            axios.get('/api/stocks')
+                .then(response => {
+                    const stocksDictionary = {};
+                    const oldStocksDictionary = this.state.stocksDictionary;
+                    const data = response.data._embedded.stocks;
+                    data.map(stock => { stocksDictionary[stock._links.self.href] = stock})
+                    this.setState({stocksDictionary, oldStocksDictionary});
+                    console.log(this.state.oldStocks);
+
+                })
+                .catch(error => {
+                     console.log(error);
+                })
+        }
+
         // invoking our API after React renders our component in the DOM
         // here we fetch an array of employees from the Spring Data REST backend,
         // and store the array in this components state data - state data is data the component handles itself
         componentDidMount() {
+
         // our function loads data using client, a Promise compliant instance of rest.js
-                client({method: 'GET', path: '/api/stocks'}).done(response => {
-                        this.setState({stocks: response.entity._embedded.stocks});
-                });
+//                client({method: 'GET', path: '/api/stocks'}).done(response => {
+//                        this.setState({stocks: response.entity._embedded.stocks});
+//                });
+                this.fetchData();
+                this.interval = setInterval( () => {
+                    this.fetchData()
+                }, 10000);
         }
 
         // render() is the API to draw our component on the screen - it's called after our state is updated above
         // stock state data is used to create a <StockList /> React component
         render() {
                 return (
-                        <TickerBoard stocks={this.state.stocks}/>
+                        <TickerBoard stocksDictionary={this.state.stocksDictionary} oldStocksDictionary = {this.state.oldStocksDictionary}/>
                 )
         }
 }
@@ -41,9 +64,13 @@ class TickerBoard extends React.Component{
                 // React components, and creating a new React component along with two properties: key, and data,
                 // which are supplied the values from employee._links.self.href and employee
                 // the "self" link is our unique key in Spring for stocks, and can be used as a unique identifier for React child nodes
-                const stocks = this.props.stocks.map(stock =>
-                        <Stock key={stock._links.self.href} stock={stock}/>
+                console.log(this.props);
+
+                const stocks = Object.keys(this.props.stocksDictionary).map(key =>
+                        <Stock key={key} stock={this.props.stocksDictionary[key]} oldStock={this.props.oldStocksDictionary[key]}/>
                 );
+
+
                 // we'll return an HTML table wrapped around the array of stocks
                 return (
                             <table>
@@ -61,12 +88,25 @@ class TickerBoard extends React.Component{
 }
 // component for rendering a single stock
 class Stock extends React.Component{
+
+
     render() {
+
+    console.log(this.props);
+    let color = 'white';
+    if(this.props.oldStock){
+       if (this.props.oldStock.price > this.props.stock.price) {
+            color = 'red';
+       }
+       else if(this.props.oldStock.price < this.props.stock.price) {
+            color = 'green';
+       }
+      }
             return (
                     <tr>
                             <td>{this.props.stock.name}</td>
                             <td>{this.props.stock.symbol}</td>
-                            <td>{this.props.stock.price}</td>
+                            <td style= {{color}} >{Number((this.props.stock.price).toFixed(3))}</td>
                     </tr>
                 )
             }
